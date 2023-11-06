4a0afada74a73c041e239236cc5681c9c2e48fde
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2023-11-06 05:56:43 +0300

    feat: add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//function declaration 
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello 2
-        </a>
-      </header>
+    <div>
+      This is App component
+      <Rating/>
+      <Accordion/>
     </div>
   );
 }
+
+
+function Rating() {
+  return (
+    <>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </>
+  );
+}
+
+function Accordion() {
+  return (
+    <>
+      <h3>start</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  );
+}
+
+function Star() {
+  return (
+    <div>Star</div>
+  );
+}
+
+
 
 export default App;
